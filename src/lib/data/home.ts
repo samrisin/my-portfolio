e4572b9be7fe37e468eff581bb6b6c27360d89ff
samridhisingh8@gmail.com <samridhisingh8@gmail.com>
2025-09-26 10:54:34 -0700

    updating intro

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,10 +8,10 @@ export const name = 'Samridhi';
 export const lastName = 'Singh';
 
 export const description =
-	'Passionate software developer with a proven track record of designing and developing applications from concept to completion. Currently a student, continuously enhancing my technical expertise and problem-solving skills. I have a strong foundation in Java and front-end development, with hands-on experience in building scalable and user-friendly applications. My interests lie in cloud computing and distributed systems, and I am always eager to explore new technologies and improve my proficiency in modern software development practices. Committed to continuous learning and innovation, I thrive in dynamic environments where I can apply my knowledge to solve real-world challenges.';
+	'Passionate software developer with a proven track record of designing and developing applications from concept to completion. Recently graduated, I am actively seeking new grad opportunities to apply and expand my skills. I have a strong foundation in Java and front-end development, with hands-on experience in building scalable and user-friendly applications. My interests lie in cloud computing and distributed systems, and I am eager to deepen my expertise in modern software development practices. Committed to continuous learning and innovation, I thrive in dynamic environments where I can apply my knowledge to solve real-world challenges.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/samrisin' },
 	{
 		platform: Platform.Linkedin,
 		link: 'https://www.linkedin.com/'
@@ -26,7 +26,7 @@ export const links: Array<{ platform: Platform; link: string }> = [
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'samridhisingh8@gmail.com'
 	},
 	{
 		platform: Platform.Youtube,
